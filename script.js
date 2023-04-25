3cf22db858f4c37d91448aadfb3bb538ce93d2fb
Harshita <harshitavns2001@gmail.com>
2023-04-25 16:43:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
